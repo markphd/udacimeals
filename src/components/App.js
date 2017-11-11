a359b7dbb44c3cfe3681d5165624b413f547222f
Mark Philip Dawal <radical.mark@gmail.com>
2017-11-11 22:53:52 +0400

    pre-connect implementation

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,29 @@
 import React, { Component } from 'react';
 import logo from './../logo.svg';
 import './../App.css';
-import { addRecipe } from '../actions'
+import { connect } from 'react-redux'
 
 class App extends Component {
-  state = {
-    calendar: null
-  }
-  componentDidMount () {
-    const { store } = this.props
-
-    store.subscribe(() => {
-      this.setState(() => ({
-        calendar: store.getState()
-      }))
-    })
-  }
-  submitFood = () => {
-    this.props.store.dispatch(addRecipe({
-      day: 'monday',
-      meal: 'breakfast',
-      recipe: {
-        label: this.input.value
-      },
-    }))
-
-    this.input.value = ''
-  }
+  
   render() {
+    console.log(this.props)
     return (
       <div>
-        <input
-          type='text'
-          ref={(input) => this.input = input}
-          placeholder="Monday's Breakfast"
-        />
-        <button onClick={this.submitFood}>Submit</button>
-
-        <pre>
-          Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
-        </pre>
+        Hello world
       </div>
     )
   }
 }
+
+function mapStateToProps(calendar) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.assign({}, calendar[day])
+  }
+}
+
 
 export default App
