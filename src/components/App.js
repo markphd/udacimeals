d2eca989d4c48ea9fdaebf71bfc28d7204d7171e
Mark <radical.mark@gmail.com>
2017-11-15 10:28:52 +0400

    completed Redux connect

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './../logo.svg';
 import './../App.css';
 import { connect } from 'react-redux'
+import { addRecipe, removeFromCalendar } from '../actions'
 
 class App extends Component {
   
@@ -22,8 +23,15 @@ function mapStateToProps(calendar) {
     calendar: dayOrder.map((day) => ({
       day,
       meals: Object.assign({}, calendar[day])
+    }))
   }
 }
 
+function mapDispatchToProps(dispatch) {
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data))
+  }
+}
 
-export default App
+export default connect( mapStateToProps, mapDispatchToProps )(App)
